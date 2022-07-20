31f5f3ac4474be9f77edbe63b00662575f524c0d
David Ye <dave.z.ye@gmail.com>
2022-07-20 20:05:09 +0200

    Add license

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  basePath: process.env.BASE_PATH || "",
 }
 
 module.exports = nextConfig
