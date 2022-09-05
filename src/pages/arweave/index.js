d58ec4c1fe96d866ef3d1e51ae4aa15c618abcbe
C <cryptomography@gmail.com>
2022-09-05 12:45:22 +0200

    Test ArWeave

--- src/pages/arweave/index.js
+++ src/pages/arweave/index.js
@@ -1,34 +1,18 @@
-import { useEffect, useState, useRef } from "react";
-import Arweave from "arweave";
-import { JWKInterface } from "arweave/node/lib/wallet";
-import Transaction from "arweave/node/lib/transaction";
-// import fs from "fs";
+import { useEffect } from "react";
+
 import FileUploader from "../../components/FileUploader";
 import FundWallet from "../../components/FundWallet";
 import InitializeBundlr from "../../components/InitializeBundlr";
 
+/*
+  ToDo:
+  - Separate into a stepper.
+
+
+*/
+
 export default function Index() {
-  const arweave = Arweave.init({});
 
-  useEffect(() => {}, []);
-
-  // const arweave = Arweave.init({
-  //   host: "127.0.0.1",
-  //   port: 1984,
-  //   protocol: "http",
-  // });
-
-  /*
-  1. Create a tx
-  2. Log tx
-
-  */
-
-  const handleFileInput = (e) => {
-    e.preventDefault();
-    const file = e.target.files[0];
-    console.log(file);
-  };
 
   return (
     <div className="max-w-md">
@@ -44,10 +28,8 @@ export default function Index() {
       <InitializeBundlr />
       <FundWallet />
       <FileUploader />
-
-      <ul className="mt-2 mb-2">
-        <li>5. Link to ArNS</li>
-      </ul>
+      
+      <div>5. Link to ArNS</div>
     </div>
   );
 }
